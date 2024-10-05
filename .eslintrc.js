b210b7bf0acdfd26c91b6a2b87e8d5455111162c
Princy Bhalu <princybhalu11@gmail.com>
2024-10-06 00:49:25 +0530

    inti set up of config of project

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true, // Enable Node.js global variables
+    // other environments can be added here if needed
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React 17 doesn't require import of React
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'prettier/prettier': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">eduction ai</div>;
+}
+
+export default App;
