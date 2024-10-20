2c79fab20909985d5b48c8419ed3fc8e00b8de1b
ParasBhardava <parasbhardava26@gmail.com>
2024-10-20 18:13:49 +0530

    fixed scrolling issue

--- src/pages/ChatBot.tsx
+++ src/pages/ChatBot.tsx
@@ -138,71 +138,74 @@ const ChatBot: React.FC = () => {
               {isChatOpen ? <AnglesDown /> : <AnglesUp />}
             </button>
           </div>
-          {isChatOpen && (
-            <div ref={chatSectionRef} className="chat-section">
-              {tempFunc()}
-              {responseObj.data.messages.map((data, index) => {
-                return (
-                  <>
-                    {data.role === 'assistant' ? (
-                      <div className="ai-chat-message">
-                        <div className="ai-chat-avatar-with-icon">
-                          <div className="ai-chat-avatar">AI</div>
-                          <div className="ai-chat-icon">
-                            <div>
-                              <PenToSquare />
-                            </div>
-                            <div>
-                              <PenToSquare />
-                            </div>
-                          </div>
-                        </div>
-                        <div className="ai-chat-text">{data.content}</div>
-                      </div>
-                    ) : (
-                      <div className="user-chat-message">
-                        <div className="user-chat-avatar-with-icon">
-                          <div className="user-chat-icon">
-                            <div>
-                              <PenToSquare />
-                            </div>
-                          </div>
-                        </div>
-                        <div className="user-chat-text">{data.content}</div>
-                        <div className="user-chat-avatar-with-icon">
-                          {/* TODO: First later of user name */}
-                          <div className="user-chat-avatar">U</div>
-                        </div>
-                      </div>
-                    )}
-                  </>
-                );
-              })}
-            </div>
-          )}
 
-          {/* TODO: Add gradient color for footer */}
-          <div ref={ref} className="footer-input-and-send-btn">
-            <div className="footer-input-section">
-              <button className="plus-btn">
-                <PlusButton />
-              </button>
-              <input
-                type="text"
-                placeholder="Ask your doubts here..."
-                className="footer-input"
-              />
-              <button className="plus-btn">
-                <PlusButton />
-              </button>
-              <button className="plus-btn">
-                <PlusButton />
-              </button>
-            </div>
-            <div className="send-container">
-              <button className="send-btn">
-                <ArrowUp />
-              </button>
+          <div className="chat-section-main">
+            {isChatOpen && (
+              <div ref={chatSectionRef} className="chat-section">
+                {tempFunc()}
+                {responseObj.data.messages.map((data, index) => {
+                  return (
+                    <>
+                      {data.role === 'assistant' ? (
+                        <div className="ai-chat-message">
+                          <div className="ai-chat-avatar-with-icon">
+                            <div className="ai-chat-avatar">AI</div>
+                            <div className="ai-chat-icon">
+                              <div>
+                                <PenToSquare />
+                              </div>
+                              <div>
+                                <PenToSquare />
+                              </div>
+                            </div>
+                          </div>
+                          <div className="ai-chat-text">{data.content}</div>
+                        </div>
+                      ) : (
+                        <div className="user-chat-message">
+                          <div className="user-chat-avatar-with-icon">
+                            <div className="user-chat-icon">
+                              <div>
+                                <PenToSquare />
+                              </div>
+                            </div>
+                          </div>
+                          <div className="user-chat-text">{data.content}</div>
+                          <div className="user-chat-avatar-with-icon">
+                            {/* TODO: First later of user name */}
+                            <div className="user-chat-avatar">U</div>
+                          </div>
+                        </div>
+                      )}
+                    </>
+                  );
+                })}
+              </div>
+            )}
+
+            {/* TODO: Add gradient color for footer */}
+            <div ref={ref} className="footer-input-and-send-btn">
+              <div className="footer-input-section">
+                <button className="plus-btn">
+                  <PlusButton />
+                </button>
+                <input
+                  type="text"
+                  placeholder="Ask your doubts here..."
+                  className="footer-input"
+                />
+                <button className="plus-btn">
+                  <PlusButton />
+                </button>
+                <button className="plus-btn">
+                  <PlusButton />
+                </button>
+              </div>
+              <div className="send-container">
+                <button className="send-btn">
+                  <ArrowUp />
+                </button>
+              </div>
             </div>
           </div>
         </div>
