073b452e1d069ce02a17c5e0bb020aad2fd73905
Princy Bhalu <princybhalu11@gmail.com>
2024-10-21 21:27:11 +0530

    merge coded by paras in main file

--- src/pages/learning-path/LearningChatView.tsx
+++ src/pages/learning-path/LearningChatView.tsx
@@ -31,6 +31,10 @@ export interface Message {
 }
 
 const LearningChatView: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const chatSectionRef = useRef<HTMLDivElement | null>(null);
+  const arrowRef = useRef<HTMLButtonElement | null>(null);
+  const [arrowButtonBottomHeight, setArrowButtonBottomHeight] = useState(0);
   const [isChatOpen, setIsChatOpen] = useState(false);
   const { type, relevantId, topicName } = useParams();
   const navigate = useNavigate();
@@ -39,6 +43,7 @@ const LearningChatView: React.FC = () => {
   const [mainContent, setMainContent] = useState(null);
   const [chatContent, setChatContent] = useState(null);
   const doutRef = useRef();
+  const [num, setNum] = useState(0);
 
   const toggleChat = () => {
     setIsChatOpen(!isChatOpen);
@@ -130,6 +135,27 @@ const LearningChatView: React.FC = () => {
     tempApiCall().then();
   }, []);
 
+  useEffect(() => {
+    if (arrowRef.current && ref.current) {
+      const arrowButtonRect = arrowRef.current.getBoundingClientRect();
+      const footerRect = ref.current.getBoundingClientRect();
+      console.log('arrowButtonRect:', arrowButtonRect);
+      console.log('footerRect:', footerRect);
+      const distanceFromBottom = footerRect.y - arrowButtonRect.y;
+      setArrowButtonBottomHeight(distanceFromBottom);
+      console.log('Distance from bottom of arrow button:', distanceFromBottom);
+      if (chatSectionRef.current) {
+        chatSectionRef.current.style.height = `${distanceFromBottom}px`;
+      }
+    }
+  }, [num]);
+  const tempFunc = () => {
+    if (num === 0) {
+      setNum(1);
+    }
+    return <></>;
+  };
+
   console.log({ chatContent });
 
   return (
@@ -177,84 +203,91 @@ const LearningChatView: React.FC = () => {
           >
             <div className="footer">
               <div className="footer-arrow-up">
-                <button className="arrow-btn" onClick={toggleChat}>
+                <button
+                  ref={arrowRef}
+                  className="arrow-btn"
+                  onClick={toggleChat}
+                >
                   {isChatOpen ? <AnglesDown /> : <AnglesUp />}
                 </button>
               </div>
-              {isChatOpen && (
-                <div className="chat-section">
-                  {chatContent &&
-                    //@ts-ignore
-                    chatContent.length > 0 &&
-                    //@ts-ignore
-                    chatContent.map((data, index) => {
-                      return (
-                        <>
-                          {data.role === 'assistant' ? (
-                            <div className="ai-chat-message">
-                              <div className="ai-chat-avatar-with-icon">
-                                <div className="ai-chat-avatar">AI</div>
-                                <div className="ai-chat-icon">
-                                  <div>
-                                    <PenToSquare />
-                                  </div>
-                                  <div>
-                                    <PenToSquare />
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="ai-chat-text">
-                                <Markdown>{data.content}</Markdown>{' '}
-                              </div>
-                            </div>
-                          ) : (
-                            <div className="user-chat-message">
-                              <div className="user-chat-avatar-with-icon">
-                                <div className="user-chat-icon">
-                                  <div>
-                                    <PenToSquare />
+              <div className="chat-section-main">
+                {isChatOpen && (
+                  <div ref={chatSectionRef} className="chat-section">
+                    {tempFunc()}
+                    {chatContent &&
+                      //@ts-ignore
+                      chatContent.length > 0 &&
+                      //@ts-ignore
+                      chatContent.map((data, index) => {
+                        return (
+                          <>
+                            {data.role === 'assistant' ? (
+                              <div className="ai-chat-message">
+                                <div className="ai-chat-avatar-with-icon">
+                                  <div className="ai-chat-avatar">AI</div>
+                                  <div className="ai-chat-icon">
+                                    <div>
+                                      <PenToSquare />
+                                    </div>
+                                    <div>
+                                      <PenToSquare />
+                                    </div>
                                   </div>
                                 </div>
+                                <div className="ai-chat-text">
+                                  <Markdown>{data.content}</Markdown>{' '}
+                                </div>
                               </div>
-                              <div className="user-chat-text">
-                                {data.content}
+                            ) : (
+                              <div className="user-chat-message">
+                                <div className="user-chat-avatar-with-icon">
+                                  <div className="user-chat-icon">
+                                    <div>
+                                      <PenToSquare />
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="user-chat-text">
+                                  {data.content}
+                                </div>
+                                <div className="user-chat-avatar-with-icon">
+                                  {/* TODO: First later of user name */}
+                                  <div className="user-chat-avatar">U</div>
+                                </div>
                               </div>
-                              <div className="user-chat-avatar-with-icon">
-                                {/* TODO: First later of user name */}
-                                <div className="user-chat-avatar">U</div>
-                              </div>
-                            </div>
-                          )}
-                        </>
-                      );
-                    })}
-                </div>
-              )}
+                            )}
+                          </>
+                        );
+                      })}
+                  </div>
+                )}
 
-              {/* TODO: Add gradient color for footer */}
-              <div className="footer-input-and-send-btn">
-                <div className="footer-input-section">
-                  <button className="plus-btn">
-                    <PlusButton />
-                  </button>
-                  <input
-                    type="text"
-                    placeholder="Ask your doubts here..."
-                    className="footer-input"
-                    //@ts-ignore
-                    ref={doutRef}
-                  />
-                  <button className="plus-btn">
-                    <PlusButton />
-                  </button>
-                  <button className="plus-btn">
-                    <PlusButton />
-                  </button>
-                </div>
-                <div className="send-container">
-                  <button className="send-btn" onClick={() => submitDout()}>
-                    <ArrowUp />
-                  </button>
+                {/* TODO: Add gradient color for footer */}
+                <div ref={ref} className="footer-input-and-send-btn">
+                  <div className="footer-input-section">
+                    <button className="plus-btn">
+                      <PlusButton />
+                    </button>
+                    <input
+                      type="text"
+                      placeholder="Ask your doubts here..."
+                      className="footer-input"
+                      // //@ts-ignore
+                      // ref={doutRef}
+                    />
+                    <button className="plus-btn">
+                      <PlusButton />
+                    </button>
+                    <button className="plus-btn">
+                      <PlusButton />
+                    </button>
+                  </div>
+                  <div className="send-container">
+                    <button className="send-btn" onClick={() => submitDout()}>
+                      <ArrowUp />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
